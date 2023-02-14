65012f3dfaf047a04ba1ccda517d855194b769f3
Christoffer Sannes <christoffer.sannes@statnett.no>
2023-02-14 14:41:37 +0100

    page name

--- src/init_app.py
+++ src/init_app.py
@@ -9,12 +9,6 @@ from st_pages import show_pages_from_config
 # app config
 st.set_page_config(layout='wide', menu_items={'About': "# Elhub statistikk. Skrive noe mer?"},
                    initial_sidebar_state="collapsed")
-no_sidebar_style = """
-    <style>
-        div[data-testid="stSidebarNav"] {display: none;}
-    </style>
-"""
-st.markdown(no_sidebar_style, unsafe_allow_html=True)
 
 # init pages in src/
 show_pages_from_config()
